95393e38d4115189274a729d4cca62bcf722645f
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2018-12-06 20:14:06 +0100

    Add tool arguments

---
+++ MarkdownCodeEmbed/InputArgs.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+
+namespace MarkdownCodeEmbed
+{
+    public class InputArgs
+    {
+        [Option("input", Required = true, HelpText = "Input markdown directory for processing.")]
+        public string InputDirectory { get; set; }
+
+        [Option("output", Required = true, HelpText = "Output directory for processed markdown.")]
+        public string OutputDirectory { get; set; }
+
+        [Option("code", Required = true, HelpText = "Directory with source code.")]
+        public string SourceCodeDirectory { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"{nameof(InputDirectory)}: {InputDirectory}");
+            Console.WriteLine($"{nameof(SourceCodeDirectory)}: {SourceCodeDirectory}");
+            Console.WriteLine($"{nameof(OutputDirectory)}: {OutputDirectory}");
+        }
+    }
+}
--- MarkdownCodeEmbed/Program.cs
+++ MarkdownCodeEmbed/Program.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Reflection;
 
 namespace MarkdownCodeEmbed
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<InputArgs>(args)
+                .WithParsed(inputArgs => new Program().Run(inputArgs));
+        }
+
+        public void Run(InputArgs args)
         {
             Console.WriteLine($"Welcome to {nameof(MarkdownCodeEmbed)} tool!");
             Console.WriteLine("Version: " + Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
+            args.Print();
         }
     }
 }
