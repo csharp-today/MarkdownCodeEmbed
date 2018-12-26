0b031598dbf98551f769227ae6a0d87dd41dbc04
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2018-12-26 18:43:35 +0100

    Embed code in markdown files

--- MarkdownCodeEmbed.Test/ApplicationTest.cs
+++ MarkdownCodeEmbed.Test/ApplicationTest.cs
@@ -10,21 +10,52 @@ namespace MarkdownCodeEmbed.Test
 {
     public class ApplicationTest : TestContainer
     {
+        private const string InputDirectory = @"C:\input\dir";
+
+        private readonly IInputContainer _inputContainer;
+
+        private InputArgs _args = new InputArgs { InputDirectory = InputDirectory };
+
+        public ApplicationTest()
+        {
+            _inputContainer = Kernel.Get<IInputContainer>();
+            Kernel.Get<IInputContainerFactory>().GetInputContainer(InputDirectory).Returns(_inputContainer);
+        }
+
+        [Fact]
+        public void Embed_Code_In_Every_Input_File() => LucidTest
+            .DefineExpected(new[] { new MarkdownFile("", "", ""), new MarkdownFile("", "", "") })
+            .Arrange(inputFiles =>
+            {
+                _inputContainer.GetMarkdownFiles().Returns(inputFiles);
+
+                var codeContainer = Kernel.Get<ICodeContainer>();
+                codeContainer.EmbedCode(null).ReturnsForAnyArgs(new MarkdownFile("", "", ""));
+
+                const string CodeDirectory = @"C:\code\dir";
+                Kernel.Get<ICodeContainerFactory>().GetCodeContainer(CodeDirectory).Returns(codeContainer);
+
+                _args = new InputArgs
+                {
+                    InputDirectory = InputDirectory,
+                    SourceCodeDirectory = CodeDirectory
+                };
+            })
+            .Act(RunApplication)
+            .Assert(inputFiles =>
+            {
+                foreach (var inputFile in inputFiles)
+                {
+                    Kernel.Get<ICodeContainer>().Received().EmbedCode(inputFile);
+                }
+            });
+
         [Fact]
         public void Get_Markdown_Input_Files() => LucidTest
-            .Arrange(() =>
-            {
-                var inputContainer = Kernel.Get<IInputContainer>();
-                inputContainer.GetMarkdownFiles().Returns(new MarkdownFile[0]);
-
-                const string InputDirectory = @"C:\input\dir";
-                Kernel.Get<IInputContainerFactory>().GetInputContainer(InputDirectory).Returns(inputContainer);
-
-                return new InputArgs { InputDirectory = InputDirectory };
-            })
+            .Arrange(() => { _inputContainer.GetMarkdownFiles().Returns(new MarkdownFile[0]); })
             .Act(RunApplication)
             .Assert(() => Kernel.Get<IInputContainer>().Received().GetMarkdownFiles());
 
-        private void RunApplication(InputArgs args) => Kernel.Get<Application>().Run(args);
+        private void RunApplication() => Kernel.Get<Application>().Run(_args);
     }
 }
