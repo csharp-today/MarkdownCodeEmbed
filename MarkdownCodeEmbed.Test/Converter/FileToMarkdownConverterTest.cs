993f7be8421fc3ce13331f42b219a9b13e03149b
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-01-21 19:33:14 +0100

    Add simple test for FileToMarkdownConverter

---
+++ MarkdownCodeEmbed.Test/Converter/FileToMarkdownConverterTest.cs
@@ -0,0 +1,21 @@
+﻿using LucidCode;
+using MarkdownCodeEmbed.Converter;
+using Ninject;
+using Shouldly;
+using Xunit;
+
+namespace MarkdownCodeEmbed.Test.Converter
+{
+    public class FileToMarkdownConverterTest : TestContainer
+    {
+        [Theory]
+        [InlineData("file.cs"), InlineData("other.xxx")]
+        public void Always_Return_CSharp_Markdown(string fileName) => LucidTest
+            .DefineExpected(@"```csharp
+TEST
+```")
+            .Arrange(_ => "TEST")
+            .Act(content => Kernel.Get<FileToMarkdownConverter>().GetMarkdown(fileName, content))
+            .Assert((expected, result) => result.ShouldBe(expected));
+    }
+}
