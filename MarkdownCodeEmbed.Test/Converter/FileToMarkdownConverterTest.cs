e81f10b5bacda8ea02eb7f164cd5f1a3002b74c8
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-01-22 20:10:56 +0100

    Not C# code generates default markdown code blocks

--- MarkdownCodeEmbed.Test/Converter/FileToMarkdownConverterTest.cs
+++ MarkdownCodeEmbed.Test/Converter/FileToMarkdownConverterTest.cs
@@ -1,21 +1,54 @@
 ﻿using LucidCode;
+using LucidCode.LucidTestFundations;
 using MarkdownCodeEmbed.Converter;
 using Ninject;
 using Shouldly;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace MarkdownCodeEmbed.Test.Converter
 {
+    public static class FileToMarkdownConverterTestExtensions
+    {
+        public const string ExpectedCode = "SOME_TEST_CODE";
+
+        public static void AssertCodeBlockIsRecognizedAs(this AssertManager<string> manager, string expectedBlockType)
+        {
+            var lines = manager.ActResult.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            lines.Length.ShouldBe(3);
+
+            const string CodeBlockMarker = "```";
+            var firstLine = lines.First();
+            firstLine.StartsWith(CodeBlockMarker).ShouldBeTrue();
+
+            var blockType = firstLine.Replace(CodeBlockMarker, "");
+            blockType.ShouldBe(expectedBlockType);
+
+            var secondLine = lines.Skip(1).First();
+            secondLine.ShouldBe(ExpectedCode);
+
+            var lastLine = lines.Last();
+            lastLine.ShouldBe(CodeBlockMarker);
+        }
+    }
+
     public class FileToMarkdownConverterTest : TestContainer
     {
-        [Theory]
-        [InlineData("file.cs"), InlineData("other.xxx")]
-        public void Always_Return_CSharp_Markdown(string fileName) => LucidTest
-            .DefineExpected(@"```csharp
-TEST
-```")
-            .Arrange(_ => "TEST")
-            .Act(content => Kernel.Get<FileToMarkdownConverter>().GetMarkdown(fileName, content))
-            .Assert((expected, result) => result.ShouldBe(expected));
+        [Fact]
+        public void Return_CSharp_Markdown() => LucidTest
+            .Arrange(GetDefaultCodeBlock)
+            .Act(code => GetMarkdown("file.cs", code))
+            .AssertCodeBlockIsRecognizedAs("csharp");
+
+        [Fact]
+        public void Return_Unknown_Markdown() => LucidTest
+            .Arrange(GetDefaultCodeBlock)
+            .Act(code => GetMarkdown("other.xxx", code))
+            .AssertCodeBlockIsRecognizedAs("");
+
+        private string GetDefaultCodeBlock() => FileToMarkdownConverterTestExtensions.ExpectedCode;
+
+        private string GetMarkdown(string fileName, string content) => Kernel.Get<FileToMarkdownConverter>().GetMarkdown(fileName, content);
     }
 }
--- MarkdownCodeEmbed/Converter/FileToMarkdownConverter.cs
+++ MarkdownCodeEmbed/Converter/FileToMarkdownConverter.cs
@@ -1,12 +1,30 @@
-﻿namespace MarkdownCodeEmbed.Converter
+﻿using System.IO.Abstractions;
+
+namespace MarkdownCodeEmbed.Converter
 {
     internal class FileToMarkdownConverter : IFileToMarkdownConverter
     {
+        private readonly IFileSystem _fileSystem;
+
+        public FileToMarkdownConverter(IFileSystem fileSystem) => _fileSystem = fileSystem;
+
         public string GetMarkdown(string filePath, string fileContent)
         {
-            return $@"```csharp
+            var type = GetCodeBlockType(filePath);
+            return $@"```{type}
 {fileContent}
 ```";
+        }
+
+        private string GetCodeBlockType(string filePath)
+        {
+            switch(_fileSystem.Path.GetExtension(filePath).ToLower())
+            {
+                case ".cs":
+                    return "csharp";
+                default:
+                    return null;
+            }
         }
     }
 }
